89a3143f8256f5afd760da9d917385d1e7692a09
Dennis de Klerk <dmdeklerk@gmail.com>
2017-05-12 21:59:57 +0200

    Microservice HTTP route manager

--- src/lib/microservice.ts
+++ src/lib/microservice.ts
@@ -35,6 +35,12 @@ declare namespace microservice {
     method: string;
     param: lang.IStringHashMap<string>;
   }
+  interface ApiRoute {
+    method: string;
+    segments: Array<string>;
+    propertyKey: string;
+    target: any;
+  }
 }
 
 module microservice {
@@ -42,7 +48,7 @@ module microservice {
   /* MicroService annotation */
   export function MicroService(name: string) {
     return function (target) {
-      microservice.getServiceManager().addService(name, function () {
+      microservice.manager().addService(name, function () {
         function F(args): void {
           return target.apply(this, args);
         }
@@ -53,16 +59,13 @@ module microservice {
   }  
 
   /* Base class for all MicroService annotated classes */
-  export abstract class AbstractMicroService<T> {
-    protected settings: T;
-    constructor(protected config: microservice.Config) {
-      this.settings = <T> config.settings;
-    }
+  export abstract class AbstractMicroService {
+    public destroy() {}
   }  
 
-  /* Access the ServiceManager */
-  export function getServiceManager(): ServiceManager {
-    return this['serviceManager'] || (this['serviceManager'] = new ServiceManager());
+  /* Access the Service Manager */
+  export function manager(): ServiceManager {
+    return this['_manager'] || (this['_manager'] = new ServiceManager());
   }
 
   class Service {
@@ -89,9 +92,129 @@ module microservice {
       if (!service) {
         return 1;
       }
-      this.running.push(service.factory.call(null, config));
+      this.running.push(this.createService(name, config));
       return 0;
+    }
+
+    /* Create and return a service instance */
+    public createService(name: string, config: lang.IStringHashMap<any>): any {
+      let service = this.services[name];
+      let instance = service.factory.call(null, config);
+      if (instance["__routes"]) {
+        instance["__routes"].forEach(route=>{
+          microservice.routes().register({
+            method: route.method,
+            segments: route.segments,
+            propertyKey: route.propertyKey,
+            target: instance
+          })
+        });
+      }
+      return instance;
     }
   }
 
+  /* Api annotation */
+  export function Api(method: string, path: string) {
+    (function validate_path(path: string) {
+      let segments = path.split('/');
+      let foundNonStatic = false;
+      segments.forEach((segment)=> {
+        if (segment.charAt(0) == ":") {
+          foundNonStatic = true;
+        }
+        else {
+          if (foundNonStatic) {
+            throw new Error(`Invalid path ${path} constant segment cannot follow a parameter segment`);
+          }
+        }
+      });
+    })(path);
+    (function validate_method(method: string) {
+      if (method != 'GET' && method != 'POST') {
+        throw new Error(`Invalid method ${method}, must be GET or POST`);
+      }
+    })(method);
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+      let routes = target["__routes"] || (target["__routes"]=[]);
+      routes.push({
+        method: method,
+        segments: path.split('/'),
+        propertyKey: propertyKey
+      });
+    };
+  }  
+
+  /* Access the Route Manager */
+  export function routes(): RouteManager {
+    return this['_routes'] || (this['_routes'] = new RouteManager());
+  }  
+
+  class RouteManager {
+    public routes: Array<ApiRoute> = [];
+
+    public register(route: ApiRoute) {
+      this.routes.push(route);
+    }
+
+    public invoke(method: string, segments: Array<string>, body: string) {
+      let route = this.findRoute(method, segments);
+      if (!util.isDefined(route)) {
+        return null;
+      }
+      // collect the parameters to pass to the microservice instance method
+      let params = [];
+      for (let i=0; i<route.segments.length; i++) {
+        let segment = route.segments[i];
+        if (segment.charAt(0)==':') {
+          params.push(segments[i]);
+        }
+      }
+      if (method=='POST') {
+        params.push(body);
+      }
+
+      try {
+        // invoke the method on the target class
+        let fn = route.target[route.propertyKey];
+        return fn.apply(route.target, params);
+      } catch(e) {
+        console.log("ERROR ROUTE INVOKE "+e);
+      }
+    }
+
+    private findRoute(method: string, segments: Array<string>): ApiRoute {
+      outer:
+      for (let i=0; i<this.routes.length; i++) {
+        let route = this.routes[i];
+        // method must match GET or POST
+        if (route.method != method) {
+          continue;
+        }
+        // segment count must match
+        if (route.segments.length != segments.length) {
+          continue;
+        }
+        // path segments must match
+        for (let j=0; j<route.segments.length; j++) {
+          let segment = route.segments[j];          
+          // required parameter segment (starts with :)
+          if (segment.charAt(0) == ":") {
+            if (!util.isDefined(segments[j])) {
+              continue outer;
+            }
+          }
+          // static segment
+          else {
+            if (segment != segments[j]) {
+              continue outer;
+            }
+          }
+        }
+        // found a match
+        return route;
+      }
+      return null;
+    }
+  }
 }
