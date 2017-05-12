c5648b65c5fdd7cfa68846db06d681e628531cec
Dennis de Klerk <dmdeklerk@gmail.com>
2017-05-12 21:58:12 +0200

    Gulp build and test scripts

--- gulpfile.js
+++ gulpfile.js
@@ -25,13 +25,63 @@ require('es6-promise').polyfill(); /* usemin requires this */
 var gulp = require('gulp');
 var util = require('gulp-util');
 var chalk = require('chalk');
+var _ = require('underscore');
+var heatledger;
+try {
+  heatledger = require('./heatledger.json');
+} catch (e) {}
+
+// There are three 'modes' in which we can run and test our microservices
+//
+//    * --mode main     Will start and connect heatledger on the main network
+//    * --mode test     Will start and connect heatledger on the test network
+//    * --mode fake     Will start heatledger on a fake one-time blockchain,
+//                      in this mode you must use the heat.driver to manually
+//                      generate blocks.
+//                      You have access to a funded account with the secretphrase
+//                      'fake' (without quotes) which has 10,000,000 fake HEAT.
+//
+// When no mode is provided fake mode is assumed.
+var modes = {
+  main: {
+    "heat.replicatorEnabled": "true",
+    "heat.enableAPIServer": "true"
+  },
+  test: {
+    "heat.replicatorEnabled": "true",
+    "heat.enableAPIServer": "true",
+    "heat.isTestnet": "true",
+    "heat.numberOfForkConfirmations": "0"    
+  },
+  fake: {
+    "heat.replicatorEnabled": "true",
+    "heat.enableAPIServer": "true",
+    "heat.isTestnet": "true",
+    "heat.blockchainDir": "microservice-test",     
+    "heat.isTestnet": "true",
+    "heat.deleteBlockchain": "true",
+    "heat.deleteHeatDb": "true",
+    "heat.isOffline": "true",
+    "heat.isSilent": "false",
+    "heat.numberOfForkConfirmations": "0",
+    "heat.shareMyAddress": "false",
+    "heat.disableGenerateBlocksThread": "true",
+    "heat.disableGetMoreBlocksThread": "true",
+    "heat.enableFakeForging": "true",
+    "heat.savePeers": "false",
+    "heat.disableProcessTransactionsThread": "true",
+    "heat.disableRemoveUnconfirmedTransactionsThread": "true",
+    "heat.disableRebroadcastTransactionsThread": "true",
+    "heat.disablePeerUnBlacklistingThread": "true",
+    "heat.getMorePeers": "false"    
+  }  
+};
 
 var PATHS = {
   src: [
     'src/*.ts',
     'src/**/*.ts'
-  ],
-  heatdir: "../heatledger/build/install/heatledger"
+  ]
 };
 
 gulp.task('clean', function () {
@@ -54,6 +104,30 @@ gulp.task('ts2js', function () {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('verify', function (cb) {
+  var path = require('path');
+  var fs = require('fs');
+  if (!heatledger) {
+    util.log(chalk.red.bold("ERROR")+" you did not create a heatledger.json file");
+  }
+  else if (!heatledger.dir) {
+    util.log(chalk.red.bold("ERROR")+" missing 'dir' property in heatledger.json, make sure it points to HEAT server installation directory");
+  }
+  else if (!fs.existsSync(heatledger.dir)) {
+    util.log(chalk.red.bold("ERROR")+" heatledger.json 'dir' path value points to non existing file");
+  }
+  else {
+    var isWin = /^win/.test(process.platform);
+    var heatbin = isWin ? path.join(heatledger.dir, 'bin', 'heatledger.bat') : path.join(heatledger.dir, 'bin', 'heatledger');
+    if (!fs.existsSync(heatbin)) {
+      util.log(chalk.red.bold("ERROR")+" could not find "+heatbin+" in installation directory");
+    }
+    else {
+      util.log(chalk.green.bold("VERIFIED")+" you are good to go!");
+    }
+  }
+});
+
 /**
  * To test a microservice we write a javascript test script which is passed
  * to the heatledger binary as its -run paramater.
@@ -62,7 +136,7 @@ gulp.task('ts2js', function () {
  * 
  * Usage: 
  * 
- *    `gulp -run "script.to.execute()" --heatdir /foo/bar --verbose`
+ *    `gulp test --run "script.to.execute()" --mode fake`
  * 
  * Parameters:
  * 
@@ -78,21 +152,30 @@ gulp.task('ts2js', function () {
  * 
  *    --run         The script to execute, must be surrounded in quotes ("..")
  * 
+ *    --config      The path to a json document whose contents will be encoded to a single line string
+ *                  and are passed to HEAT server as the --properties argument, each property in the 
+ *                  json object will be added to HEAT configuration properties.
+ * 
  *    --verbose     When provided will also include all console output from heat server,
  *                  otherwise only console output from your script is shown.
+ * 
+ *    --mode        Either; main, test or fake (default)
  */
 gulp.task('test', ['build'], function (cb) {
   var argv = require('yargs').argv;
   var exec = require('child_process').exec;
   var spawn = require('child_process').spawn;
   var path = require('path');
+  var conf = modes[argv.mode||'fake'];
+  var properties = getProperties(argv.config, conf);
   
   var scriptsDir = path.join(__dirname, 'dist');
   var isWin = /^win/.test(process.platform);
   var heatbin = isWin ? "bin\heatledger.bat" : "bin/heatledger";
-  var heatdir = argv.heatdir || PATHS.heatdir;
+  var heatdir = (heatledger&&heatledger.dir) || argv.heatdir;
   var sourcedir = path.join(__dirname, 'src');
-  var server = spawn(heatbin, ["-path", scriptsDir, "-run", argv.run, "-sourcedir", sourcedir], { cwd: heatdir });
+  var args = ["-path", scriptsDir, "-run", argv.run, "-sourcedir", sourcedir, "-properties", properties];
+  var server = spawn(heatbin, args, { cwd: heatdir });
 
   function logVerbose(msg) {
     if (!msg || msg.trim().length == 0) return;
@@ -133,6 +216,31 @@ gulp.task('test', ['build'], function (cb) {
 
   server.stdout.on('data', (data) => { log(data+"") });
   server.stderr.on('data', (data) => { log(data+"") });
+});
+
+function getProperties(configPath, conf) {
+  var properties = null;
+  try {
+    properties = require(configPath)||{};
+  } catch (e) {
+    console.log("Either not provided or cant load --config path "+configPath);
+  }  
+  var temp = {};
+  _.extendOwn(temp, conf);
+  _.extendOwn(temp, properties);
+  properties = JSON.stringify(temp);
+  return properties;
+}
+
+gulp.task('inject', ['build'], function (cb) {
+  var del = require('del');
+  var path = require('path');
+  var distDir = path.join(__dirname, 'dist');
+  var heatdir = "/home/dirk/git/heatledger";
+  var scriptsDir = path.join(heatdir, 'scripts');
+  del([scriptsDir+"/*"]);
+  return gulp.src(['dist/*'])
+    .pipe(gulp.dest(scriptsDir));  
 });
 
 gulp.task('build', ['clean','ts2js']);
